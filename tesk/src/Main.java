34ca71f9673d624292dc85901498a3f0dfaecd2c
Aden Yusuf <aadan1535@gmail.com>
2025-10-08 16:15:14 -0700

    rashka second commit

--- tesk/src/Main.java
+++ tesk/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         university uv = new university("iuea university","Kansanga",2000);
-        students st = new students("nuur",77," design");
+        students st = new students("deeqow",7567," design");
         uv.showuniversityDetail();
         st.showstudentsDetail();
 
