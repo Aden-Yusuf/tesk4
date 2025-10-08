08d80f6d9e95dc217a981975e99ed8447bdf2b4a
Aden Yusuf <aadan1535@gmail.com>
2025-10-08 16:00:01 -0700

    second commit

--- tesk/src/Main.java
+++ tesk/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         university uv = new university("iuea university","Kansanga",2000);
-        students st = new students("aaden",33333,"oop");
+        students st = new students("Ahmed",767677,"web design");
         uv.showuniversityDetail();
         st.showstudentsDetail();
 
